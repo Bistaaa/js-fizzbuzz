ae45b2e41ff0c269ef25c5bd597c6528e7c775c0
Bistaaa <michele.bistarelli.mb@outlook.it>
2023-05-02 17:45:02 +0200

    numeri e quadrati

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 //selezionare container html
+const container = document.getElementById("container");
 
 //creare ciclo di operazioni
+for (let i = 0; i <=1000; i++) {
+    
+    //creazione nuovo elemento
+    const quadrato = document.createElement("span");
 
-//creazione nuovo elemento
+    //aggiungiamo una classe per gli span
+    quadrato.classList.add("square");
 
-//inserimento valore nell'elemento
+    //inserimento valore nell'elemento
+    quadrato.append(i);
 
-//inseriamo nel continer il nuovo elemento
+    //inseriamo nel continer il nuovo elemento
+    container.append(quadrato);
+
+
+}
+
